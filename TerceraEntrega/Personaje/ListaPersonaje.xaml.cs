e192d4b2c83f2a3bc1ab35fb797cffaeb8218725
tgmez <31938817+tgmez@users.noreply.github.com>
2017-12-03 12:36:56 -0300

    Se agrega la lista de personajes

---
+++ TerceraEntrega/Personaje/ListaPersonaje.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TerceraEntrega.BL;
+
+namespace TerceraEntrega.Personaje
+{
+	/// <summary>
+	/// Lógica de interacción para ListaPersonaje.xaml
+	/// </summary>
+	public partial class ListaPersonaje : Page
+	{
+		public ListaPersonaje()
+		{
+			InitializeComponent();
+			ListaPersonajes.ItemsSource = PersonajeBL.Listar();
+		}
+	}
+}
